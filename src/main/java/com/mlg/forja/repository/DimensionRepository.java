88ea4fe10f408b8b6f530e05efb30b1b31a93a67
mgondon12 <134943849+mgondon12@users.noreply.github.com>
2026-06-25 17:22:51 -0400

    Delete src directory

--- src/main/java/com/mlg/forja/repository/DimensionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mlg.forja.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mlg.forja.modelo.Dimension;
-
-public interface DimensionRepository extends JpaRepository<Dimension, Integer> {
-}
